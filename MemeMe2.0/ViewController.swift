86e41a069c28a3670513084d2569d5ae71fcc5c1
s.danyal.k@gmail.com <Laynad@123>
2018-12-03 22:48:14 -0700

    Initial Commit

---
+++ MemeMe2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe2.0
+//
+//  Created by SDK on 12/3/18.
+//  Copyright © 2018 SDK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
